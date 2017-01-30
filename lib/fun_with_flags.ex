354b88a687b807997bf4c1cd74a371230702cf58
tom <tommaso@pavese.me>
2017-01-30 02:26:10 +0000

    generated new mix project with supervisor

---
+++ lib/fun_with_flags.ex
@@ -0,0 +1,18 @@
+defmodule FunWithFlags do
+  @moduledoc """
+  Documentation for FunWithFlags.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FunWithFlags.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fun_with_flags_test.exs
@@ -0,0 +1,8 @@
+defmodule FunWithFlagsTest do
+  use ExUnit.Case
+  doctest FunWithFlags
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
