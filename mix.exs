f69882a021660193fc3e1edb825b329306ad5c94
tom <tommaso@pavese.me>
2017-01-31 02:31:45 +0000

    fix ex_doc options

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule FunWithFlags.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.14.5", only: :docs},
+      {:ex_doc, "~> 0.14.5", only: :dev},
     ]
   end
 
