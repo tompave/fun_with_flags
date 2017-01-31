ba828cdace3319b7f48f786b668fdb4cbe549a7f
tom <tommaso@pavese.me>
2017-01-31 02:27:44 +0000

    more readme, mixfile data, license

--- README.md
+++ README.md
@@ -2,14 +2,33 @@
 
 FunWithFlags, the Elixir feature flag library.
 
+## Usage
+
+Still a work in progress, expect breaking changes.
+
+```elixir
+FunWithFlags.enabled?(:cool_new_feature)
+false
+
+{:ok, true} = FunWithFlags.enable(:cool_new_feature)
+
+FunWithFlags.enabled?(:cool_new_feature)
+true
+
+{:ok, false} = FunWithFlags.disable(:cool_new_feature)
+
+FunWithFlags.enabled?(:cool_new_feature)
+false
+```
+
+
 ## Installation
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `fun_with_flags` to your list of dependencies in `mix.exs`:
+The package can be installed by adding `fun_with_flags` to your list of dependencies in `mix.exs`:
 
 ```elixir
 def deps do
-  [{:fun_with_flags, "~> 0.1.0"}]
+  [{:fun_with_flags, "~> 0.0.1"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -2,14 +2,17 @@ defmodule FunWithFlags.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :fun_with_flags,
-     name: "FunWithFlags",
-     source_url: "https://github.com/tompave/fun_with_flags",
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :fun_with_flags,
+      source_url: "https://github.com/tompave/fun_with_flags",
+      version: "0.0.1",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      description: description(),
+      package: package(),
+    ]
   end
 
   # Configuration for the OTP application
@@ -33,6 +36,27 @@ defmodule FunWithFlags.Mixfile do
   defp deps do
     [
       {:ex_doc, "~> 0.14.5", only: :docs},
+    ]
+  end
+
+
+  defp description do
+    """
+    FunWithFlags, the Elixir feature flag library.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Tommaso Pavese"
+      ],
+      licenses: [
+        "MIT"
+      ],
+      links: %{
+        "GitHub" => "https://github.com/tompave/fun_with_flags",
+      }
     ]
   end
 end
