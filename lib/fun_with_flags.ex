31e890c7e9376c58a4e505e47d6e605868e14e1d
tom <tommaso@pavese.me>
2017-01-31 01:32:48 +0000

    Added failing tests and interface for base functionality.

--- lib/fun_with_flags.ex
+++ lib/fun_with_flags.ex
@@ -1,18 +1,61 @@
 defmodule FunWithFlags do
   @moduledoc """
-  Documentation for FunWithFlags.
+  FunWithFlags, the Elixir feature flag library.
   """
 
   @doc """
-  Hello world.
+  Checks if a flag is enabled.
 
   ## Examples
 
-      iex> FunWithFlags.hello
-      :world
+      iex> FunWithFlags.enabled?(:foobar)
+      false
+      iex> FunWithFlags.enabled?(:querty)
+      true
+  """
+  @spec enabled?(atom) :: boolean
+  def enabled?(flag_name) when is_atom(flag_name) do
+    false
+  end
+
+
+
+  @doc """
+  Enables a feature flag.
+
+  ## Examples
+
+      iex> FunWithFlags.enabled?(:foobar)
+      false
+      iex> {:ok, true} = FunWithFlags.enable(:foobar)
+      {:ok, true}
+      iex> FunWithFlags.enabled?(:foobar)
+      true
 
   """
-  def hello do
-    :world
+  @spec enable(atom) :: {:ok, true}
+  def enable(flag_name) when is_atom(flag_name) do
+    {:ok, true}
   end
+
+
+
+  @doc """
+  Disables a feature flag.
+
+  ## Examples
+
+      iex> FunWithFlags.enabled?(:foobar)
+      true
+      iex> {:ok, false} = FunWithFlags.disable(:foobar)
+      {:ok, false}
+      iex> FunWithFlags.enabled?(:foobar)
+      false
+
+  """
+  @spec disable(atom) :: {:ok, false}
+  def disable(flag_name) when is_atom(flag_name) do
+    {:ok, false}
+  end
+
 end
--- test/fun_with_flags_test.exs
+++ test/fun_with_flags_test.exs
@@ -1,8 +1,40 @@
 defmodule FunWithFlagsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest FunWithFlags
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "enabled?" do
+    test "it returns false for non existing feature flags" do
+      assert false == FunWithFlags.enabled?(:i_do_not_exits)
+    end
+
+    test "it returns false for a disabled feature flag" do
+      FunWithFlags.disable(:foobar)
+      assert false == FunWithFlags.enabled?(:foobar)
+    end
+
+    test "it returns true for an enabled feature flag" do
+      FunWithFlags.enable(:barbaz)
+      assert true == FunWithFlags.enabled?(:barbaz)
+    end
+  end
+
+
+  test "flags can be enabled and disabled" do
+    assert false == FunWithFlags.enabled?(:domodossola)
+    FunWithFlags.enable(:domodossola)
+    assert true == FunWithFlags.enabled?(:domodossola)
+    FunWithFlags.disable(:domodossola)
+    assert false == FunWithFlags.enabled?(:domodossola)
+  end
+
+
+  test "enabling always returns the tuple {:ok, true} on success" do
+    assert {:ok, true} = FunWithFlags.enable(:perugia)
+    assert {:ok, true} = FunWithFlags.enable(:perugia)
+  end
+
+  test "disabling always returns the tuple {:ok, false} on success" do
+    assert {:ok, false} = FunWithFlags.disable(:norcia)
+    assert {:ok, false} = FunWithFlags.disable(:norcia)
   end
 end
