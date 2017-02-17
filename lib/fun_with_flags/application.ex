00e18560ef99052f5f5813a8d486b63e302b515a
tom <tommaso@pavese.me>
2017-02-17 00:42:39 +0000

    only start the notifications supervisor if the cache is enabled

--- lib/fun_with_flags/application.ex
+++ lib/fun_with_flags/application.ex
@@ -4,14 +4,23 @@ defmodule FunWithFlags.Application do
   use Application
 
   def start(_type, _args) do
+    opts = [strategy: :one_for_one, name: FunWithFlags.Supervisor]
+    Supervisor.start_link(children(), opts)
+  end
+
+
+  defp children do
     import Supervisor.Spec, warn: false
 
-    children = [
-      supervisor(FunWithFlags.Store.Supervisor, [], restart: :permanent),
-      worker(FunWithFlags.Notifications, [], restart: :permanent),
-    ]
-
-    opts = [strategy: :one_for_one, name: FunWithFlags.Supervisor]
-    Supervisor.start_link(children, opts)
+    if FunWithFlags.Config.cache? do
+      [
+        supervisor(FunWithFlags.Store.Supervisor, [], restart: :permanent),
+        worker(FunWithFlags.Notifications, [], restart: :permanent),
+      ]
+    else
+      [
+        supervisor(FunWithFlags.Store.Supervisor, [], restart: :permanent),
+      ]
+    end
   end
 end
