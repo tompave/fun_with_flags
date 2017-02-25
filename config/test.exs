e059c239aea494f260ecab6759976b64b3338969
Tommaso Pavese <tommaso@pavese.me>
2017-02-25 14:54:43 +0000

    explicitly test withthe right redis db

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,9 @@ use Mix.Config
 
 IO.puts "Loading config for default test env"
 
+config :fun_with_flags, :redis,
+  database: 5
+
 config :fun_with_flags, :cache,
   enabled: true,
   ttl: 60
--- config/test_no_cache.exs
+++ config/test_no_cache.exs
@@ -2,5 +2,8 @@ use Mix.Config
 
 IO.puts "Loading config for the \"no cache\" integration test env"
 
+config :fun_with_flags, :redis,
+  database: 5
+
 config :fun_with_flags, :cache,
   enabled: false
