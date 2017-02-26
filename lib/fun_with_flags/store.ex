81a5524410c26d80e014f20985baf8727719c352
Tommaso Pavese <tommaso@pavese.me>
2017-02-26 16:38:29 +0000

    Updates the store module to work with flags and gates

--- lib/fun_with_flags/store.ex
+++ lib/fun_with_flags/store.ex
@@ -6,30 +6,29 @@ defmodule FunWithFlags.Store do
 
   def lookup(flag_name) do
     case Cache.get(flag_name) do
+      {:ok, flag} -> flag
       {:miss, reason, stale_value_or_nil} ->
         case Persistent.get(flag_name) do
+          {:ok, flag} ->
+            Cache.put(flag) 
+            flag
           {:error, _reason} ->
             try_to_use_the_cached_value(reason, stale_value_or_nil)
-          bool when is_boolean(bool) ->
-            # {:ok, ^bool} = Cache.put(flag_name, bool)
-            # swallow cache errors for the moment
-            Cache.put(flag_name, bool) 
-            bool
         end
-      {:ok, value} ->
-        value
     end
   end
 
 
-  defp try_to_use_the_cached_value(:not_found, nil), do: false
   defp try_to_use_the_cached_value(:expired, value), do: value
+  defp try_to_use_the_cached_value(_, _) do
+    raise "Can't load feature flag"
+  end
 
 
-  def put(flag_name, value) do
-    case Persistent.put(flag_name, value) do
-      {:ok, ^value} ->
-        Cache.put(flag_name, value)
+  def put(flag_name, gate) do
+    case Persistent.put(flag_name, gate) do
+      {:ok, flag} ->
+        Cache.put(flag)
       {:error, _reason} = error ->
         error
     end
@@ -39,11 +38,10 @@ defmodule FunWithFlags.Store do
   def reload(flag_name) do
     # IO.puts "reloading #{flag_name}"
     case Persistent.get(flag_name) do
+      {:ok, flag} ->
+        Cache.put(flag)
       {:error, _reason} = error ->
         error
-      bool when is_boolean(bool) ->
-        Cache.put(flag_name, bool) 
-        {:ok, bool}
     end
   end
 end
--- test/fun_with_flags/store_test.exs
+++ test/fun_with_flags/store_test.exs
@@ -3,197 +3,193 @@ defmodule FunWithFlags.StoreTest do
   import FunWithFlags.TestUtils
   import Mock
 
-  alias FunWithFlags.{Store, Config}
+  alias FunWithFlags.{Store, Config, Flag, Gate}
+  alias FunWithFlags.Store.{Cache, Persistent}
 
   setup_all do
     on_exit(__MODULE__, fn() -> clear_redis_test_db() end)
     :ok
   end
 
+  setup do
+    Cache.flush()
+    name = unique_atom()
+    gate = %Gate{type: :boolean, enabled: true}
+    flag = %Flag{name: name, gates: [gate]}
+    {:ok, name: name, gate: gate, flag: flag}
+  end
+
 
   describe "lookup(flag_name)" do
-    test "looking up an undefined flag returns false" do
+    test "looking up an undefined flag returns a flag with no gates" do
       flag_name = unique_atom()
-      assert false == Store.lookup(flag_name)
+      assert %Flag{name: ^flag_name, gates: []} = Store.lookup(flag_name)
     end
 
-    test "looking up a defined flag returns its value" do
-      flag_name = unique_atom()
-
-      Store.put(flag_name, true)
-      assert true == Store.lookup(flag_name)
-
-      Store.put(flag_name, false)
-      assert false == Store.lookup(flag_name)
+    test "looking up a defined flag returns the flag", %{name: name, gate: gate, flag: flag} do
+      assert %Flag{name: ^name, gates: []} = Store.lookup(name)
+      Store.put(name, gate)
+      assert ^flag = Store.lookup(name)
     end
   end
 
 
   describe "put(flag_name, value)" do
-    test "put() can change the value of a flag" do
-      flag_name = unique_atom()
+    test "put() can change the value of a flag", %{name: name, gate: gate, flag: flag} do
+      assert %Flag{name: ^name, gates: []} = Store.lookup(name)
 
-      assert false == Store.lookup(flag_name)
-      Store.put(flag_name, true)
-      assert true == Store.lookup(flag_name)
-      Store.put(flag_name, false)
-      assert false == Store.lookup(flag_name)
+      Store.put(name, gate)
+      assert ^flag = Store.lookup(name)
+
+      gate2 = %Gate{gate | enabled: false}
+      Store.put(name, gate2)
+      assert %Flag{name: ^name, gates: [^gate2]} = Store.lookup(name)
+      refute match? ^flag, Store.lookup(name)
     end
 
-    test "put() returns the tuple {:ok, a_boolean_value}" do
-      flag_name = unique_atom()
-      assert {:ok, true} == Store.put(flag_name, true)
-      assert {:ok, false} == Store.put(flag_name, false)
+    test "put() returns the tuple {:ok, %Flag{}}", %{name: name, gate: gate, flag: flag} do
+      assert {:ok, ^flag} = Store.put(name, gate)
     end
   end
 
 
   describe "reload(flag_name) reads the flag value from Redis and updates the Cache" do
-    alias FunWithFlags.Store.{Cache, Persistent}
+    test "if the flag is not found in Redis, it sets it to false in the Cache", %{name: name, flag: flag} do
+      empty_flag = %Flag{name: name, gates: []}
+      assert {:ok, ^empty_flag} = Persistent.get(name)
+      assert {:miss, :not_found, nil} = Cache.get(name)
+      assert ^empty_flag = Store.lookup(name)
 
-    test "if the flag is not found in Redis, it sets it to false in the Cache" do
-      flag_name = unique_atom()
-      assert false == Persistent.get(flag_name)
+      Cache.put(flag)
+      assert {:ok, ^flag} = Cache.get(name)
+      assert ^flag = Store.lookup(name)
 
-      Cache.put(flag_name, true)
-      assert {:ok, true} = Cache.get(flag_name)
-      assert true = Store.lookup(flag_name)
+      Store.reload(name)
 
-      Store.reload(flag_name)
-
-      assert {:ok, false} = Cache.get(flag_name)
-      assert false == Store.lookup(flag_name)
+      assert {:ok, ^empty_flag} = Cache.get(name)
+      assert ^empty_flag = Store.lookup(name)
     end
 
-    test "if the flag is false in Redis, it sets it to false in the Cache" do
-      flag_name = unique_atom()
-      Persistent.put(flag_name, false)
-      assert false == Persistent.get(flag_name)
 
-      Cache.put(flag_name, true)
-      assert {:ok, true} = Cache.get(flag_name)
-      assert true == Store.lookup(flag_name)
 
-      Store.reload(flag_name)
+    test "if the flag is stored in Redis, it stores it in the Cache", %{name: name, gate: gate, flag: flag} do
+      {:ok, ^flag} = Persistent.put(name, gate)
+      assert {:ok, ^flag} = Persistent.get(name)
 
-      assert {:ok, false} = Cache.get(flag_name)
-      assert false == Store.lookup(flag_name)
-    end
+      gate2 = %Gate{gate | enabled: false}
+      flag2 = %Flag{name: name, gates: [gate2]}
 
-    test "if the flag is true in Redis, it sets it to true in the Cache" do
-      flag_name = unique_atom()
-      Persistent.put(flag_name, true)
-      assert true == Persistent.get(flag_name)
+      Cache.put(flag2)
+      assert {:ok, ^flag2} = Cache.get(name)
+      assert ^flag2 = Store.lookup(name)
+      refute match? ^flag, Store.lookup(name)
 
-      Cache.put(flag_name, false)
-      assert {:ok, false} = Cache.get(flag_name)
-      assert false == Store.lookup(flag_name)
+      Store.reload(name)
 
-      Store.reload(flag_name)
-
-      assert {:ok, true} = Cache.get(flag_name)
-      assert true == Store.lookup(flag_name)
+      assert {:ok, ^flag} = Cache.get(name)
+      assert ^flag = Store.lookup(name)
+      refute match? ^flag2, Store.lookup(name)
     end
   end
 
 
-  describe "unit: enable and disable with this module's API" do
-    test "looking up a disabled flag returns false" do
-      flag_name = unique_atom()
-      Store.put(flag_name, false)
-      assert false == Store.lookup(flag_name)
-    end
+  # describe "integration: enable and disable with the top-level API" do
+  #   test "looking up a disabled flag returns false" do
+  #     flag_name = unique_atom()
+  #     FunWithFlags.disable(flag_name)
+  #     assert false == Store.lookup(flag_name)
+  #   end
 
-    test "looking up an enabled flag returns true" do
-      flag_name = unique_atom()
-      Store.put(flag_name, true)
-      assert true == Store.lookup(flag_name)
-    end
-  end
-
-
-  describe "integration: enable and disable with the top-level API" do
-    test "looking up a disabled flag returns false" do
-      flag_name = unique_atom()
-      FunWithFlags.disable(flag_name)
-      assert false == Store.lookup(flag_name)
-    end
-
-    test "looking up an enabled flag returns true" do
-      flag_name = unique_atom()
-      FunWithFlags.enable(flag_name)
-      assert true == Store.lookup(flag_name)
-    end
-  end
+  #   test "looking up an enabled flag returns true" do
+  #     flag_name = unique_atom()
+  #     FunWithFlags.enable(flag_name)
+  #     assert true == Store.lookup(flag_name)
+  #   end
+  # end
 
 
   describe "integration: Cache and Persistence" do
-    alias FunWithFlags.Store.{Cache, Persistent}
+    test "if we have a Cached value, the Persistent store is not touched at all", %{name: name, flag: flag} do
+      Cache.put(flag)
 
-    test "setting a value will update both the cache and the persistent store" do
-      flag_name = unique_atom()
+      with_mocks([
+        {Persistent, [:passthrough], []},
+        {Cache, [:passthrough], []}
+      ]) do
+        assert ^flag = Store.lookup(name)
+        assert called(Cache.get(name))
+        refute called(Persistent.get(name))
+      end
+    end
 
-      assert {:miss, :not_found, nil} == Cache.get(flag_name)
-      assert false == Persistent.get(flag_name)
-      Store.put(flag_name, true)
-      assert {:ok, true} == Cache.get(flag_name)
-      assert true == Persistent.get(flag_name)
+
+
+    test "setting a value will update both the cache and the persistent store", %{name: name, gate: gate, flag: flag} do
+      empty_flag = %Flag{name: name, gates: []}
+
+      assert {:miss, :not_found, nil} == Cache.get(name)
+      assert {:ok, ^empty_flag} = Persistent.get(name)
+
+      Store.put(name, gate)
+      assert {:ok, ^flag} = Cache.get(name)
+      assert {:ok, ^flag} = Persistent.get(name)
     end
 
     test "when the value is initially not in the cache but set in redis,
-          looking it up will populate the cache" do
-      flag_name = unique_atom()
-      Persistent.put(flag_name, true)
+          looking it up will populate the cache", %{name: name, gate: gate, flag: flag} do
+      Persistent.put(name, gate)
 
-      assert {:miss, :not_found, nil} == Cache.get(flag_name)
-      assert true == Persistent.get(flag_name)
+      assert {:miss, :not_found, nil} = Cache.get(name)
+      assert {:ok, ^flag} = Persistent.get(name)
       
-      assert true == Store.lookup(flag_name)
-      assert {:ok, true} == Cache.get(flag_name)
+      assert ^flag = Store.lookup(name)
+      assert {:ok, ^flag} = Cache.get(name)
     end
 
 
     test "when the value is initially not in the cache and not in redis,
-          looking it up will populate the cache" do
-      flag_name = unique_atom()
+          looking it up will populate the cache", %{name: name} do
+      empty_flag = %Flag{name: name, gates: []}
 
-      assert {:miss, :not_found, nil} == Cache.get(flag_name)
-      assert false == Persistent.get(flag_name)
-      
-      assert false == Store.lookup(flag_name)
-      assert {:ok, false} == Cache.get(flag_name)
+      assert {:miss, :not_found, nil} == Cache.get(name)
+      assert {:ok, ^empty_flag} = Persistent.get(name)
+
+      assert ^empty_flag = Store.lookup(name)
+      assert {:ok, ^empty_flag} = Cache.get(name)
     end
 
 
-    test "put() will change both the value stored in the Cache and in Redis" do
-      flag_name = unique_atom()
+    test "put() will change both the value stored in the Cache and in Redis", %{name: name, gate: gate, flag: flag} do
+      {:ok, ^flag} = Persistent.put(name, gate)
+      {:ok, ^flag} = Cache.put(flag)
 
-      {:ok, false} = Persistent.put(flag_name, false)
-      {:ok, false} = Cache.put(flag_name, false)
+      assert {:ok, ^flag} = Cache.get(name)
+      assert {:ok, ^flag} = Persistent.get(name)
 
-      assert {:ok, false} = Cache.get(flag_name)
-      assert false == Persistent.get(flag_name)
+      gate2 = %Gate{gate | enabled: false}
+      flag2 = %Flag{name: name, gates: [gate2]}
 
-      {:ok, true} = Store.put(flag_name, true)
+      {:ok, ^flag2} = Store.put(name, gate2)
 
-      assert {:ok, true} = Cache.get(flag_name)
-      assert true == Persistent.get(flag_name)
+      assert {:ok, ^flag2} = Cache.get(name)
+      assert {:ok, ^flag2} = Persistent.get(name)
     end
 
 
-    test "when a value in the cache expires, we can still reload it from redis" do
-      flag_name = unique_atom()
-      Persistent.put(flag_name, true)
+    test "when a value in the cache expires, it will load it from redis
+          and update the cache", %{name: name, gate: gate, flag: flag} do
+      Persistent.put(name, gate)
 
-      assert {:miss, :not_found, nil} == Cache.get(flag_name)
-      assert true == Persistent.get(flag_name)
+      assert {:miss, :not_found, nil} = Cache.get(name)
+      assert {:ok, ^flag} = Persistent.get(name)
 
-      assert true == Store.lookup(flag_name)
-      assert {:ok, true} == Cache.get(flag_name)
+      assert ^flag = Store.lookup(name)
+      assert {:ok, ^flag} = Cache.get(name)
 
       timetravel by: (Config.cache_ttl + 1) do
-        assert {:miss, :expired, true} = Cache.get(flag_name)
-        assert true == Store.lookup(flag_name)
+        assert {:miss, :expired, ^flag} = Cache.get(name)
+        assert ^flag = Store.lookup(name)
+        assert {:ok, ^flag} = Cache.get(name)
       end
     end
   end
@@ -202,51 +198,56 @@ defmodule FunWithFlags.StoreTest do
   describe "in case of Persistent store failure" do
     alias FunWithFlags.Store.{Cache, Persistent}
 
-    test "if we have a Cached value, the Persistent store is not touched at all" do
-      flag_name = unique_atom()
-      Cache.put(flag_name, true)
+    test "if we have a Cached value, the Persistent store is not touched at all", %{name: name, flag: flag} do
+      Cache.put(flag)
 
-      with_mock(Persistent, [:passthrough], []) do
-        assert true == Store.lookup(flag_name)
-        refute called(Persistent.get(flag_name))
+      with_mocks([
+        {Persistent, [], get: fn(^name) -> {:error, "mocked error"} end},
+        {Cache, [:passthrough], []}
+      ]) do
+        assert ^flag = Store.lookup(name)
+        assert called(Cache.get(name))
+        refute called(Persistent.get(name))
       end
     end
 
 
-    test "if the Cached value is expired, it will still be used" do
-      flag_name = unique_atom()
+    test "if the Cached value is expired, it will still be used", %{name: name, gate: gate, flag: flag} do
+      Persistent.put(name, gate)
+      assert ^flag = Store.lookup(name)
 
-      Persistent.put(flag_name, false)
-      assert false == Store.lookup(flag_name)
+      gate2 = %Gate{gate | enabled: false}
+      flag2 = %Flag{name: name, gates: [gate2]}
 
-      Cache.put(flag_name, true)
-      assert {:ok, true} = Cache.get(flag_name)
+      Cache.put(flag2)
+      assert {:ok, ^flag2} = Cache.get(name)
 
       timetravel by: (Config.cache_ttl + 1) do
-        with_mock(Persistent, [], get: fn(^flag_name) -> {:error, "mocked error"} end) do
-          assert {:miss, :expired, true} = Cache.get(flag_name)
-          assert true == Store.lookup(flag_name)
-          assert called(Persistent.get(flag_name))
-          assert {:error, "mocked error"} = Persistent.get(flag_name)
+        with_mock(Persistent, [], get: fn(^name) -> {:error, "mocked error"} end) do
+          assert ^flag2 = Store.lookup(name)
+          assert {:miss, :expired, ^flag2} = Cache.get(name)
+          assert called(Persistent.get(name))
+          assert {:error, "mocked error"} = Persistent.get(name)
         end
       end
     end
 
 
-    test "if there is no cached value, it defaults to false" do
-      flag_name = unique_atom()
-
-      Persistent.put(flag_name, true)
-      assert true == Store.lookup(flag_name)
+    test "if there is no cached value, it raises an error", %{name: name, gate: gate, flag: flag} do
+      Persistent.put(name, gate)
+      assert ^flag = Store.lookup(name)
 
       Cache.flush()
-      assert {:miss, :not_found, nil} = Cache.get(flag_name)
+      assert {:miss, :not_found, nil} = Cache.get(name)
 
-      with_mock(Persistent, [], get: fn(^flag_name) -> {:error, "mocked error"} end) do
-        assert false == Store.lookup(flag_name)
-        assert called(Persistent.get(flag_name))
-        assert {:error, "mocked error"} = Persistent.get(flag_name)
+      with_mock(Persistent, [], get: fn(^name) -> {:error, "mocked error"} end) do
+        assert_raise RuntimeError, "Can't load feature flag", fn() ->
+          Store.lookup(name)
+        end
+        assert called(Persistent.get(name))
+        assert {:error, "mocked error"} = Persistent.get(name)
       end
     end
   end
+
 end
