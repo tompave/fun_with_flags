35bc7bba783b75832651060ca29a5ac274532f8a
tom <tommaso@pavese.me>
2017-02-28 02:31:10 +0000

    no need to validate the type in SimpleStore

--- lib/fun_with_flags/simple_store.ex
+++ lib/fun_with_flags/simple_store.ex
@@ -2,12 +2,11 @@ defmodule FunWithFlags.SimpleStore do
   @moduledoc false
 
   alias FunWithFlags.Store.Persistent
-  alias FunWithFlags.Flag
 
 
   def lookup(flag_name) do
     case Persistent.get(flag_name) do
-      {:ok, flag = %Flag{}} -> {:ok, flag}
+      {:ok, flag} -> {:ok, flag}
       error                 -> error
     end
   end
