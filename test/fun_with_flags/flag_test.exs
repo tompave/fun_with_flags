7c1575086e2c9121048547840de9a4e9eb4e974d
tom <tommaso@pavese.me>
2017-03-14 23:52:30 +0000

    more tests for the flag module

--- test/fun_with_flags/flag_test.exs
+++ test/fun_with_flags/flag_test.exs
@@ -29,7 +29,7 @@ defmodule FunWithFlags.FlagTest do
   end
 
 
-  describe "enabled?(flag)" do
+  describe "enabled?(flag) - only flag parameter, no options" do
     test "it returns true if the flag has a boolean value = true" do
       flag = %Flag{name: :banana, gates: [Gate.new(:boolean, true)]}
       assert Flag.enabled?(flag)
@@ -43,6 +43,77 @@ defmodule FunWithFlags.FlagTest do
     test "it returns false if the flag doesn't have any gate" do
       flag = %Flag{name: :banana, gates: []}
       refute Flag.enabled?(flag)
+    end
+
+    test "other gates are ignored" do
+      data = %{mario: "luigi", actor_id: "peach"}
+      gates = [Gate.new(:actor, data, false), Gate.new(:boolean, true)]
+      flag = %Flag{name: :banana, gates: gates}
+      assert Flag.enabled?(flag)
+    end
+  end
+
+
+
+  describe "enabled?(flag, for: actor)" do
+    alias FunWithFlags.TestUser
+
+    setup do
+      john = %TestUser{id: 42, email: "john@snow.nw"}
+      arya = %TestUser{id: 151, email: "arya@stark.wf"}
+      {:ok, john: john, arya: arya}
+    end
+
+
+    test "with no gates, checking for an actor default to false", %{john: john, arya: arya} do
+      flag = %Flag{name: :pear, gates: []}
+
+      refute Flag.enabled?(flag, for: john)
+      refute Flag.enabled?(flag, for: arya)
+      refute Flag.enabled?(flag)
+    end
+
+
+    test "with only boolean gates, checking for an actor falls back to the boolean gate", %{john: john, arya: arya} do
+      flag = %Flag{name: :pear, gates: [Gate.new(:boolean, true)]}
+
+      assert Flag.enabled?(flag, for: john)
+      assert Flag.enabled?(flag, for: arya)
+      assert Flag.enabled?(flag)
+
+      flag = %Flag{name: :pear, gates: [Gate.new(:boolean, false)]}
+
+      refute Flag.enabled?(flag, for: john)
+      refute Flag.enabled?(flag, for: arya)
+      refute Flag.enabled?(flag)
+    end
+
+
+    test "with only actor gates, the actors are checked and otherwise it defaults to false", %{john: john, arya: arya} do
+      flag = %Flag{name: :pear, gates: [Gate.new(:actor, john, true)]}
+      assert Flag.enabled?(flag, for: john)
+      refute Flag.enabled?(flag, for: arya)
+      refute Flag.enabled?(flag)
+
+      flag = %Flag{name: :pear, gates: [Gate.new(:actor, john, false), Gate.new(:actor, arya, true)]}
+      refute Flag.enabled?(flag, for: john)
+      assert Flag.enabled?(flag, for: arya)
+      refute Flag.enabled?(flag)
+    end
+
+
+    test "an actor gate takes precendence over the boolean gate, when enabling", %{john: john, arya: arya} do
+      flag = %Flag{name: :pear, gates: [Gate.new(:boolean, false), Gate.new(:actor, john, true)]}
+      assert Flag.enabled?(flag, for: john)
+      refute Flag.enabled?(flag, for: arya)
+      refute Flag.enabled?(flag)
+    end
+
+    test "an actor gate takes precendence over the boolean gate, when disabling", %{john: john, arya: arya} do
+      flag = %Flag{name: :pear, gates: [Gate.new(:boolean, true), Gate.new(:actor, john, false)]}
+      refute Flag.enabled?(flag, for: john)
+      assert Flag.enabled?(flag, for: arya)
+      assert Flag.enabled?(flag)
     end
   end
 end
