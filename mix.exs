1edff3526152edfbea3917a35e5ee0db01e8275a
tom <tommaso@pavese.me>
2017-04-11 09:16:45 +0100

    updated redix_pubsub dependency to 0.3.0, which adds support for redix >0.5.1

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule FunWithFlags.Mixfile do
       {:ex_doc, "~> 0.15", only: :dev},
       {:mock, "~> 0.2.1", only: [:test, :test_no_cache]},
       {:redix, "~> 0.5.2"},
-      {:redix_pubsub, "~> 0.2.0"},
+      {:redix_pubsub, "~> 0.3.0"},
     ]
   end
 
