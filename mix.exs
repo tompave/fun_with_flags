7b58f55a3ced33f184d70795dfae0e78ec571d66
tom <tommaso@pavese.me>
2017-05-22 22:21:52 +0100

    upgraded redix and redix_pubsub dependencies

--- mix.exs
+++ mix.exs
@@ -42,8 +42,8 @@ defmodule FunWithFlags.Mixfile do
     [
       {:ex_doc, "~> 0.15", only: :dev},
       {:mock, "~> 0.2.1", only: [:test, :test_no_cache]},
-      {:redix, "~> 0.5.2"},
-      {:redix_pubsub, "~> 0.3.0"},
+      {:redix, "~> 0.6.0"},
+      {:redix_pubsub, "~> 0.4.0"},
     ]
   end
 
