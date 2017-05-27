2ca017cdafe6d73e681acbc9e922026c50bd0556
tom <tommaso@pavese.me>
2017-05-27 13:59:31 +0100

    Added fail-and-retry support to the Phoenix.PubSub adapter, so that it is more resilient to situations where the pubsub process is not started yet, for example doring the boot time of a Phoenix app.

--- lib/fun_with_flags/notifications/phoenix_pubsub.ex
+++ lib/fun_with_flags/notifications/phoenix_pubsub.ex
@@ -6,10 +6,8 @@ defmodule FunWithFlags.Notifications.PhoenixPubSub do
   require Logger
   alias FunWithFlags.{Config, Store}
 
-  # Host applications will inject this. It's supposed to be
-  # an active pubsub PID or name atom.
-  @conn Config.pubsub_client()
   @channel "fun_with_flags_changes"
+  @max_attempts 5
 
 
   def worker_spec do
@@ -38,7 +36,7 @@ defmodule FunWithFlags.Notifications.PhoenixPubSub do
 
   def publish_change(flag_name) do
     Task.start fn() ->
-      Phoenix.PubSub.broadcast!(@conn, @channel,
+      Phoenix.PubSub.broadcast!(client(), @channel,
         {:fwf_changes, {:updated, flag_name, unique_id()}}
       )
     end
@@ -52,22 +50,44 @@ defmodule FunWithFlags.Notifications.PhoenixPubSub do
   # The unique_id will become the state of the GenServer
   #
   def init(unique_id) do
-    subscribe()
+    subscribe(1)
     {:ok, unique_id}
   end
 
 
-  defp subscribe do
+  defp subscribe(attempt) when attempt <= @max_attempts do
     try do
-      case Phoenix.PubSub.subscribe(@conn, @channel) do
-        :ok -> :ok
+      case Phoenix.PubSub.subscribe(client(), @channel) do
+        :ok ->
+          # All good
+          Logger.debug "FunWithFlags: Subscribed to to Phoenix.PubSub process #{inspect(client())}."
+          :ok
         {:error, reason} ->
-          Logger.error "FunWithFlags: Cannot subscribe to Phoenix.PubSub process #{inspect(@conn)} ({:error, #{inspect(reason)}})."
+          # Handled application errors
+          Logger.error "FunWithFlags: Cannot subscribe to Phoenix.PubSub process #{inspect(client())} ({:error, #{inspect(reason)}})."
+          try_again_to_subscribe(attempt)
       end
     rescue
       e ->
-        Logger.error "FunWithFlags: Cannot subscribe to Phoenix.PubSub process #{inspect(@conn)} (exception: #{inspect(e)})."
+        # The pubsub process was probably not running. This happens when using it in Phoenix, as it tries to connect the
+        # first time while the application is booting, and the Phoenix.PubSub process is not fully started yet.
+        Logger.error "FunWithFlags: Cannot subscribe to Phoenix.PubSub process #{inspect(client())} (exception: #{inspect(e)})."
+        try_again_to_subscribe(attempt)
     end
+  end
+
+
+  # We can't connect to the PubSub process. Possibly because it didn't start.
+  #
+  defp subscribe(_) do
+    raise "Tried to subscribe to Phoenix.PubSub process #{inspect(client())} #{@max_attempts} times. Giving up."
+  end
+
+
+  # Wait 1 second and try again
+  #
+  defp try_again_to_subscribe(attempt) do
+    Process.send_after(self(), {:subscribe_retry, (attempt + 1)}, 1000)
   end
 
 
@@ -87,6 +107,18 @@ defmodule FunWithFlags.Notifications.PhoenixPubSub do
     Task.start(Store, :reload, [name])
     {:noreply, unique_id}
   end
+
+
+  # When subscribing to the pubsub process fails, the process sends itself a delayed message
+  # to try again. It will be handlerd here.
+  #
+  def handle_info({:subscribe_retry, attempt}, unique_id) do
+    Logger.debug "FunWithFlags: retrying to subscribe to Phoenix.PubSub, attempt #{attempt}."
+    subscribe(attempt)
+    {:noreply, unique_id}
+  end
+
+  defp client, do: Config.pubsub_client()
 end
 
 end # Code.ensure_loaded?
