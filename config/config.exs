6e9f9574135c1bb18e3251e095e668b06fc2ae00
tom <tommaso@pavese.me>
2017-05-29 23:48:27 +0100

    Better organization of the mix config and test setup

--- config/config.exs
+++ config/config.exs
@@ -6,6 +6,41 @@ use Mix.Config
 #   [enabled: true, adapter: FunWithFlags.Notifications.Redis]
 
 
+# -------------------------------------------------
+# Extract from the ENV
+
+with_cache =
+  case System.get_env("CACHE_ENABLED") do
+    "false" -> false
+    "0"     -> false
+    _       -> true # default
+  end
+
+with_phx_pubsub =
+  case System.get_env("PUBSUB_BROKER") do
+    "phoenix_pubsub" -> true
+    _ -> false
+  end
+
+# -------------------------------------------------
+# Configuration
+
+config :fun_with_flags, :cache,
+  enabled: with_cache,
+  ttl: 60
+
+
+if with_phx_pubsub do
+  config :fun_with_flags, :cache_bust_notifications, [
+    adapter: FunWithFlags.Notifications.PhoenixPubSub,
+    client: :fwf_test
+  ]
+end
+
+
+# -------------------------------------------------
+# Import
+
 case Mix.env do
   :test -> import_config "test.exs"
   _     -> nil
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -13,10 +13,15 @@ end
 # With some configurations the tests are run with `--no-start`, because
 # we want to start the Phoenix.PubSub process before starting the application.
 Application.ensure_all_started(:fun_with_flags)
-
-IO.puts "Running tests with $TEST_OPTS='#{System.get_env("TEST_OPTS")}'"
-IO.puts "$PUBSUB_BROKER=#{System.get_env("PUBSUB_BROKER") }"
+IO.puts "--------------------------------------------------------------"
+IO.puts "$TEST_OPTS='#{System.get_env("TEST_OPTS")}'"
+IO.puts "$CACHE_ENABLED=#{System.get_env("CACHE_ENABLED")}"
+IO.puts "$PUBSUB_BROKER=#{System.get_env("PUBSUB_BROKER")}"
+IO.puts "--------------------------------------------------------------"
+IO.puts "Cache enabled:         #{inspect(FunWithFlags.Config.cache?)}"
+IO.puts "Persistence adapter:   #{inspect(FunWithFlags.Config.persistence_adapter())}"
 IO.puts "Notifications adapter: #{inspect(FunWithFlags.Config.notifications_adapter())}"
+IO.puts "--------------------------------------------------------------"
 
 FunWithFlags.TestUtils.use_redis_test_db()
 ExUnit.start()
