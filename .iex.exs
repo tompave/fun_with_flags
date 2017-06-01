87118b118805518d49233ea54246063967913457
tom <tommaso@pavese.me>
2017-06-02 00:07:26 +0100

    Don't start the dev redis connection when redis is not being used

--- .iex.exs
+++ .iex.exs
@@ -8,16 +8,16 @@ alias FunWithFlags.Store.Persistent.Ecto.Schema, as: Model
 
 if Config.persist_in_ecto? do
   {:ok, _pid} = FunWithFlags.Dev.EctoRepo.start_link()
+else
+  {:ok, redis} =
+    Redix.start_link(
+      Config.redis_config,
+      [name: :dev_console_redis, sync_connect: false])
 end
 
 if Config.phoenix_pubsub? do
   {:ok, _pid} = Phoenix.PubSub.PG2.start_link(:fwf_test, [pool_size: 1])
 end
-
-{:ok, redis} =
-  Redix.start_link(
-    Config.redis_config,
-    [name: :dev_console_redis, sync_connect: false])
 
 
 cacheinfo = fn() ->
