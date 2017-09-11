86aae4946d5df4e8e03b5d54571935256ba45670
tom <tommaso@pavese.me>
2017-09-12 00:17:55 +0100

    bumped version to 0.10.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v0.10.0
 
 Possibly Breaking Changes:
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FunWithFlags.Mixfile do
   use Mix.Project
 
-  @version "0.9.2"
+  @version "0.10.0"
 
   def project do
     [
