91970b13be960d504acdc0cc909eaf5cf42b5c4d
tom <tommaso@pavese.me>
2018-01-20 16:01:52 +0000

    Add the formatter config file

---
+++ .formatter.exs
@@ -0,0 +1,10 @@
+[
+  inputs: [
+    "config/**/*.{ex.exs}",
+    "dev_support/**/*.{ex.exs}",
+    "lib/**/*.{ex,exs}",
+    "priv/**/*.{ex,exs}",
+    "test/**/*.{ex,exs}",
+    "mix.exs",
+  ],
+]
