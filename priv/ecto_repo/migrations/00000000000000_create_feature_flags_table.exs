9fb9b70339ac8cb5170e3074e50ee2e7168ab557
Cory O'Daniel <cory@coryodaniel.com>
2018-02-08 09:45:32 -0800

    Changed ecto migration to use a natural PK
    If a project sets ecto's `migration_primary_key` to binary_id, the
    FunWithFlags Record module fails to insert the primary key because ecto
    normally autogenerates this, but `@primary_key {:id, :binary_id,
    autogenerate: true}` has to be set on the module.

--- priv/ecto_repo/migrations/00000000000000_create_feature_flags_table.exs
+++ priv/ecto_repo/migrations/00000000000000_create_feature_flags_table.exs
@@ -2,18 +2,12 @@ defmodule FunWithFlags.Dev.EctoRepo.Migrations.CreateFeatureFlagsTable do
   use Ecto.Migration
 
   def up do
-    create table(:fun_with_flags_toggles) do
-      add :flag_name, :string
-      add :gate_type, :string
-      add :target, :string
+    create table(:fun_with_flags_toggles, primary_key: false) do
+      add :flag_name, :string, primary_key: true
+      add :gate_type, :string, primary_key: true
+      add :target, :string, primary_key: true
       add :enabled, :boolean
     end
-
-    create index(
-      :fun_with_flags_toggles,
-      [:flag_name, :gate_type, :target],
-      [unique: true, name: "fwf_flag_name_gate_target_idx"]
-    )
   end
 
   def down do
