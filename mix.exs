524caf4cc7bcfaffebba2c9f8bab11fcc2f07eb5
tom <tommaso@pavese.me>
2021-11-15 02:20:09 +0100

    Add support for Elixir 1.12 and OTP 24, drop Elixir 1.8 and OTP 21.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule FunWithFlags.Mixfile do
     [
       app: :fun_with_flags,
       version: @version,
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
