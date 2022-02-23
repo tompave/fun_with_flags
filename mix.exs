d8f6026f1f507ba77176038a5a45bc73fa65d236
tom <tommaso@pavese.me>
2022-02-23 02:41:54 +0100

    Still allow postgrex 0.13

--- mix.exs
+++ mix.exs
@@ -60,7 +60,7 @@ defmodule FunWithFlags.Mixfile do
     [
       {:redix, "~> 1.0", optional: true},
       {:ecto_sql, "~> 3.0", optional: true},
-      {:postgrex, "~> 0.15.11", optional: true, only: [:dev, :test]},
+      {:postgrex, ">= 0.13.0 and < 0.16.0", optional: true, only: [:dev, :test]},
       {:myxql, "~> 0.2", optional: true, only: [:dev, :test]},
       {:phoenix_pubsub, "~> 2.0", optional: true},
 
