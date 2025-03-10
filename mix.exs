205c714c5cff918392453881230a8858c29b3005
tom <tommaso@pavese.me>
2025-03-10 23:54:07 +0100

    Add telemetry as a direct dependency

--- mix.exs
+++ mix.exs
@@ -74,6 +74,7 @@ defmodule FunWithFlags.Mixfile do
       {:postgrex, "~> 0.16", optional: true, only: [:dev, :test]},
       {:myxql, "~> 0.2", optional: true, only: [:dev, :test]},
       {:phoenix_pubsub, "~> 2.0", optional: true},
+      {:telemetry, "~> 1.3"},
 
       {:mock, "~> 0.3", only: :test},
 
